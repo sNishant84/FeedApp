3dc5170a0d01913e9722ca3145453fb5d2085b41
sNishant84 <snishant842@gmail.com>
2024-09-09 00:09:43 +0530

    Add files via upload

---
+++ src/Data/data.ts
@@ -0,0 +1,22 @@
+export const feedData=[
+    {
+        id:1,
+        name:'Theresa Web',
+        imgUrl:'/assets/avatarIcon.svg',
+        timeStamp:5,
+        postText:'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        emoji:'👋',
+        commentsCount:24,
+        isEdited:false
+    },
+    {
+        id:2,
+        name:'Marvin Mckiney',
+        imgUrl:'/assets/avatarIcon2.svg',
+        timeStamp:8,
+        postText:'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+        emoji:'😞',
+        commentsCount:18,
+        isEdited:true
+    }
+]
