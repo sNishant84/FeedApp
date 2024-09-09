9a30556ed06bdc554805b2a735cb952e1ce3e39e
sNishant84 <snishant842@gmail.com>
2024-09-09 12:00:13 +0530

    Update Post.tsx

--- src/Components/Post.tsx
+++ src/Components/Post.tsx
@@ -22,7 +22,7 @@ const Post=({setModalOpen}:PostProps):JSX.Element=>{
             <div className='px-4 py-4 border-solid border-2 border-[#35373B] rounded-lg bg-[#35373B] flex flex-col gap-3 '>
                 <h1 className='text-[#C5C7CA] text-lg'>Create Post</h1>
                 <div className='border-solid border border-[#191920] rounded-lg flex gap-2 bg-[#191920] p-3 items-center'>
-                    <div className='border-solid border w-12 h-12 border-[#27292D] bg-[#35373B] rounded-full text-center place-content-center'>💬</div>
+                    <div className='border-solid border w-8 h-8 md:w-12 md:h-12 border-[#27292D] bg-[#35373B] rounded-full text-center place-content-center'>💬</div>
                     <textarea className='text-base text-[#7F8084] w-[100%] border-none bg-[#191920] resize-none place-content-center outline-none p-2 cursor-pointer' placeholder={'How are you feeling today?'} />
                 </div>
                 <div className='flex justify-end'>
